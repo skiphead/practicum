532cb7b0548092edf12072a8b47229f19aa9e5b8
Alexey Zabolotnov <skiphead@yandex.ru>
2025-10-03 19:30:48 +0300

    Инкремент 4

--- infra/config/loader.go
+++ infra/config/loader.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"gopkg.in/yaml.v3"
 	"os"
 )
@@ -8,6 +9,15 @@ import (
 // LoadConfig загружает конфигурацию из YAML-файла
 func LoadConfig(configPath string) (*Config, error) {
 	config := &Config{}
+
+	flag.StringVar(&config.ServerAddr, "a", "127.0.0.1:8080", "Порт для запуска сервера")
+	flag.StringVar(&config.BaseURL, "b", "", "Базовый адрес результирующего сокращённого URL (например: http://localhost:8000/qsd54gFg)")
+	flag.Parse()
+
+	// Если порт передан аргументом, он имеет высший приоритет и перезаписывает конфиг
+	if config.ServerAddr != "" {
+		return config, nil
+	}
 
 	file, err := os.ReadFile(configPath)
 	if err != nil {
